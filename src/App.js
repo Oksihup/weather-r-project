6cf7398446c82016c19fc36315e407313f131910
Oksihup <107550003+Oksihup@users.noreply.github.com>
2022-10-03 15:36:07 +0300

    Added structure without forecast

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+import Weather from "./Weather";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Kyiv" />
+        <footer>
+          This project was coded by {""}
+          <a href="" target="blank" rel="noopener noreferrer">
+            Oksana Zuravska
+          </a>{" "}
+          and is {""}
+          <a href="" target="blank" rel="noopener noreferrer">
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="" target="blank" rel="noopener noreferrer">
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
